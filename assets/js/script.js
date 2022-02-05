93bc002d33591785104f8585d43e913a0b7b154b
christanfox <bayshorestinks@aol.com>
2022-02-05 11:02:59 -0500

    save to local storage functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 // Display the current date and time in the header
-
 function currentDate() {
     var currentDate = moment().format('dddd, MM/DD/YYYY - LT');        
     $("#currentDay").html(currentDate);
@@ -7,7 +6,15 @@ function currentDate() {
 currentDate();
 setInterval(currentDate, 1000); // refresh rate in milliseconds
 
-    
+                          /***************************/
+                         /*--SAVE TO LOCAL STORAGE--*/
+                        /***************************/
+var hour = document.querySelector(".hour");
 
+$(".saveBtn").click(function() {
 
+    var time = $(this).parent().attr("id")
+    var value = $(this).siblings(".description").val();
 
+localStorage.setItem(time, value)    
+});
